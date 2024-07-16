adf61147eac8027669fa0577748f3c5e79e84a2b
LuckyKumar926 <kumarlaxmangc@gmail.com>
2024-07-16 05:31:18 -0400

    Delete server.js

--- server.js
+++
@@ -1,8 +0,0 @@
-// server.js
-const http = require('http')
-const server = http.createServer((req, res) => {
-    console.log('New connection')
-    res.end("Hello world change in the code deploy ")
-})
-const PORT = process.env.PORT || 8080
-server.listen(PORT, () => console.log('Listening'))
